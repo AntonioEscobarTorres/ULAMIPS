46d7ffdc5cb2c69baede9b10efb7d37031b019df
Antônio Torres <115806133+AntonioEscobarTorres@users.noreply.github.com>
2023-06-25 21:02:28 -0300

    troca no controle

--- controle.vhd
+++ controle.vhd
@@ -10,10 +10,20 @@ entity Controle is
 end entity;
 
 architecture comportamento of Controle is
+
+	signal sig_funct_sel: std_logic_vector(2 downto 0);
+	
 begin
-    sel <= 
-        "000" when Funct = "100100" else "001" when Funct = "100101" else 
-        "010" when ULAOp = "00" or (ULAOp = "10" and Funct = "100000") else
-        "110" when ULAOp = "01" or (ULAOp = "10" and Funct = "100010") else
-        "111";
+	sig_funct_sel <= "010" when funct = "100000" else -- adição
+						  "110" when funct = "100010" else -- subtração
+						  "000" when funct = "100100" else -- and
+						  "001" when funct = "100101" else -- or
+						  "111" when funct = "101010" else -- set on less than
+					     "011";									  -- adição
+						  
+	sel <= "010" 			when ULAOp = "00" else -- adição
+			 "110" 			when ULAOp = "01" else -- subtração
+			 sig_funct_sel when ULAOp = "10" else -- seleção funct
+			 "011";                               -- erro
+
 end architecture;
